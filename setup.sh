8a24059b199226e3f4249219a930f5d855f633d9
Baptiste Zorzi <baptiste.zorzi@gmail.com>
2019-09-16 09:44:52 +0200

    adding launchWeb file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 path="`pwd`/`dirname $0`/"
 
 ln -s $path/bashrc $HOME/.bashrc
-
 ln -s $path/VScode_config "~/.config/Code - OSS/User"
+
+cp $path/httpd.template.conf /etc/httpd/conf/httpd.template.conf
+
