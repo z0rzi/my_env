2f245d6ad5e3c74ace108ab5f33bb6d6030cdae5
Baptiste Zorzi <baptiste.zorzi@gmail.com>
2019-10-19 16:20:34 +0200

    new setup and VIM integration

--- setup.sh
+++ setup.sh
@@ -1,30 +1,105 @@
 #!/bin/bash
 
-
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 path="`pwd`/`dirname $0`/"
+should_install=""
 
-ln -s $path/bashrc $HOME/.bashrc
-ln -s $path/VScode_config ~/.config/Code\ -\ OSS/User
+function errorMessage() {
+    echo "USAGE = '$0 <to_setup> (--install)'"
+    echo
+    echo "to_setup can one of the following:"
+    echo "  - all"
+    echo "  - bashrc"
+    echo "  - vim"
+    echo "  - vscode"
+    echo "  - dbeaver"
+    echo
 
-# Apache launchWeb script config
-ln -s $path/apache/files/httpd.template.conf /etc/httpd/conf/httpd.template.conf
+    exit;
+}
 
-# Vim configuration
-mv $HOME/.vim $HOME/..vim
-ln -s $path/files/vim $HOME/.vim
+function parseArgs() {
+    [ $# -lt 1 ] && errorMessage
+    export to_setup=$1
 
-# DBeaver configuration
-mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json.backup       $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.data-sources.json.backup
-mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json.backup $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.credentials-config.json.backup
-mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json              $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.data-sources.json
-mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json        $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.credentials-config.json
+    while [ $# -gt 0 ]; do
+        if [ "$1" = "--install" ]; then
+            export should_install=1
+        fi
 
-ln -s $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json       $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json.backup
-ln -s $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json.backup
-ln -s $path/files/dbeaver/data-sources.json                                              $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json
-ln -s $path/files/dbeaver/credentials-config.json                                        $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json
+        shift
+    done
+}
+
+parseArgs $@
+
+case "$to_setup" in
+    "all" | "bashrc")
+        echo "Starting bashrc setup..."
+        ln -s $path/bashrc $HOME/.bashrc
+        echo "bashrc setup completed"
+        ;;
+
+    "all" | "vscode")
+        echo "Starting VScode setup..."
+
+        ln -s $path/files/VScode ~/.config/Code\ -\ OSS/User
+
+        [ "$should_install" ] && sudo pacman --noconfirm -S code
+
+        echo "VScode setup completed"
+        ;;
+
+    "all" | "apache")
+        echo "Starting Apache setup..."
+        echo
+        echo "executing the following command as root:"
+        echo "ln -s $path/apache/files/httpd.template.conf /etc/httpd/conf/httpd.template.conf"
+        sudo ln -s $path/apache/files/httpd.template.conf /etc/httpd/conf/httpd.template.conf
+
+        [ "$should_install" ] && sudo pacman --noconfirm -S apache php php-apache
+
+        echo "Apache setup completed"
+        ;;
+
+
+    "all" | "dbeaver")
+        echo "Starting DBeaver setup..."
+
+        mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json.backup       $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.data-sources.json.backup
+        mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json.backup $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.credentials-config.json.backup
+        mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json              $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.data-sources.json
+        mv $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json        $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/.credentials-config.json
+
+        ln -s $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json       $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json.backup
+        ln -s $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json.backup
+        ln -s $path/files/dbeaver/data-sources.json                                              $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/data-sources.json
+        ln -s $path/files/dbeaver/credentials-config.json                                        $HOME/.local/share/DBeaverData/workspace6/General/.dbeaver/credentials-config.json
+
+        [ "$should_install" ] && sudo pacman --noconfirm -S jdk-openjdk dbeaver
+
+        echo "DBeaver setup completed"
+        ;;
+
+    "all" | "vim")
+        echo "Starting VIM setup..."
+
+        mv $HOME/.vim $HOME/..vim
+        ln -fs $path/files/vim $HOME/.config/nvim
+        ln -fs $path/files/vim $HOME/.vim
+        ln -fs $path/files/vim/vimrc $HOME/.vimrc
+
+        if [ "$should_install" ]; then
+            echo "Installing nvim, gvim, fzf, pip"
+            sudo pacman --noconfirm -S nvim gvim fzf pip
+            pip3 install --user pynvim
+            pip3 install --user --upgrade pynvim
+            echo "You should logout for the changes to take effect."
+            echo "You should install the font present in '$HOME/.vim/fonts/' for the icons to work properly"
+        fi
+
+        echo "VIM setup completed"
+        ;;
+    *)
+        errorMessage
+        ;;
+esac
